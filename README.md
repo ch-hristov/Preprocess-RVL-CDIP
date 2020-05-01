af76ed25d4237f5816957b3438f84f56ed93d7e8
Christo Christov <hristohristov54@gmail.com>
2020-05-01 12:03:12 +0200

    Add flatten.py and instructions

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Preprocess the RVL-CDIP dataset into a grouped by category format
 
 
 The RVL-CDIP should be downloaded from the original website - https://www.cs.cmu.edu/~aharley/rvl-cdip/
+
+1. Use the notebook to build the per category dataset
+2. This dataset contains a lot of subfolders, that's why you need to also run 
+
+`python flatten.py <main folder>`
+
+This command will flatten each subfolder of 'main folder' argument. 
---
+++ flatten.py
@@ -0,0 +1,36 @@
+import shutil
+import os.path
+import sys
+
+
+directory_target = sys.argv[1]
+
+import os
+import itertools
+import shutil
+
+def generate_filename(extension):
+    xx = str(uuid.uuid4()) + extension
+    return xx
+
+def move(destination):
+    all_files = []
+    guid = 0
+    for root, _dirs, files in itertools.islice(os.walk(destination), 1, None):
+        for filename in files:
+            final_destination = os.path.join(root,filename)
+            os.rename(final_destination,os.path.dirname(final_destination) + str(guid) + '.png')
+            all_files.append(os.path.dirname(final_destination) + str(guid) + '.png')
+            guid= guid + 1
+
+    for filename in all_files:
+        shutil.move(filename, os.path.join(destination,os.path.basename(filename)))
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir) if os.path.isdir(os.path.join(a_dir, name))]
+
+
+
+directories = get_immediate_subdirectories(directory_target)
+for dir in directories:
+    move(directory_target + os.path.sep + dir)
